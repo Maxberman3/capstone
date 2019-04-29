e81de331343e7d8d95708dc2226d32865cfaddf7
Maxberman3 <mb3ax@virginia.edu>
2019-04-29 16:14:28 -0400

    fixed sourcefile paths, fixed clang xml to excel

--- clangxml_to_excel.py
+++ clangxml_to_excel.py
@@ -7,7 +7,7 @@ from pandas import ExcelFile
 columns=['SourceFile','Bug Group','Bug Code','Bug Message','Build ID','AssesmentReportFile','Instance Location','Location ID','Primary','StartLine','EndLine']
 index=range(0,865)
 parasoftbugs=pd.DataFrame(index=index,columns=columns)
-DOMTree = xml.dom.minidom.parse("parasoft.xml")
+DOMTree = xml.dom.minidom.parse("Clang static analyzer.xml")
 collection = DOMTree.documentElement
 bugs=collection.getElementsByTagName('BugInstance')
 rowcount=0
@@ -18,7 +18,6 @@ for bug in bugs:
     bugmessage=bug.getElementsByTagName('BugMessage')[0].firstChild.nodeValue
     buildid=bug.getElementsByTagName('BugTrace')[0].getElementsByTagName('BuildId')[0].firstChild.nodeValue
     assessmentreportfile=bug.getElementsByTagName('BugTrace')[0].getElementsByTagName('AssessmentReportFile')[0].firstChild.nodeValue
-    instancelocation=bug.getElementsByTagName('BugTrace')[0].getElementsByTagName('InstanceLocation')[0].firstChild.nodeValue
     for buglocation in buglocations:
         locations=buglocation.getElementsByTagName('Location')
         for location in locations:
@@ -33,7 +32,6 @@ for bug in bugs:
             parasoftbugs.set_value(rowcount,'Bug Message',bugmessage)
             parasoftbugs.set_value(rowcount,'Build ID',buildid)
             parasoftbugs.set_value(rowcount,'AssessmentReportFile',assessmentreportfile)
-            parasoftbugs.set_value(rowcount,'Instance Location',instancelocation)
             parasoftbugs.set_value(rowcount,'Location ID',loc_id)
             parasoftbugs.set_value(rowcount,'Primary',is_primary)
             parasoftbugs.set_value(rowcount,'StartLine',startline)
