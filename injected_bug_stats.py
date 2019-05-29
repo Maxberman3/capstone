834391659d75147a43234fff2314d18befea9d34
Maxberman3 <mb3ax@virginia.edu>
2019-05-29 20:00:20 +0300

    started cataloging sql bugs

--- injected_bug_stats.py
+++ injected_bug_stats.py
@@ -7,26 +7,21 @@ cwetypes={}
 
 for index,row in truthdf.iterrows():
     bugids.add(row['Bug ID'])
-    cert1=row['CERT#1']
-    cert2=row['CERT#2']
-    cert3=row['CERT#3']
-    cert4=row['CERT #4']
-    if cert1 not in cwetypes:
-        cwetypes[cert1]=1
+    cwe1=row['CWE# 1']
+    cwe2=row['CWE# 2']
+    cwe3=row['CWE #3']
+    if cwe1 not in cwetypes:
+        cwetypes[cwe1]=1
     else:
-        cwetypes[cert1]+=1
-    if cert2 not in cwetypes:
-        cwetypes[cert2]=1
+        cwetypes[cwe1]+=1
+    if cwe2 not in cwetypes:
+        cwetypes[cwe2]=1
     else:
-        cwetypes[cert2]+=1
-    if cert3 not in cwetypes:
-        cwetypes[cert3]=1
+        cwetypes[cwe2]+=1
+    if cwe3 not in cwetypes:
+        cwetypes[cwe3]=1
     else:
-        cwetypes[cert3]+=1
-    if cert4 not in cwetypes:
-        cwetypes[cert4]=1
-    else:
-        cwetypes[cert4]+=1
+        cwetypes[cwe3]+=1
 print('The number of unique bug id\'s is '+str(len(bugids)))
 print('The total number of alerts generated from injected bugs is '+str(index+1))
 print('The number of alerts associated with each CWE type is:')
