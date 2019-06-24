d86144adecd9a7770a753aba1cd4eede2fef46cd
Maxberman3 <mb3ax@virginia.edu>
2019-06-24 16:07:02 -0400

    caught mistake in manifest check, working on building combined data table

---
+++ CombineDataframe.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from pandas import ExcelWriter,ExcelFile
+
+alerts=pd.read_excel('Juliet_Test_Suite/swampresults_edit_checked.xlsx')
+data=pd.DataFrame(columns=['Severity','CWE','Clang Alert','CodeSonar Alert','Clang Rule','CodeSonar Rule','Line','True Positive'])
+indexmap={}
+for index,row in alerts.iterrows():
+    alertpath=row['Path']
+    severity=row['Severity']
+    cwe=row['CWE']
+    rule=row['Rule']
+    line=row['Line']
+    positive=row['True Positive Flag']
+    tool=row['Tool']
+    if alertpath not in indexmap:
+        indexmap[alertpath]={line:index}
+        if tool=='Clang':
+            data.append({'Severity':severity,'CWE':cwe,'Clang Alert':1,'CodeSonar Alert':0,'Clang Rule':rule,'CodeSonar Rule':'N/A','Line':line,'True Positive':positive})
+        elif tool=='gt-csonar':
+            data.append({'Severity':'N/A','CWE':'N/A','Clang Alert':0,'CodeSonar Alert':1,'Clang Rule':'N/A','CodeSonar Rule':rule,'Line':line,'True Positive':positive})
+    elif line not in indexmap[alertpath]:
+        indexmap[alertpath].update({line:index})
+        if tool=='Clang':
+            data.append({'Severity':severity,'CWE':cwe,'Clang Alert':1,'CodeSonar Alert':0,'Clang Rule':rule,'CodeSonar Rule':'N/A','Line':line,'True Positive':positive})
+        elif tool=='gt-csonar':
+            data.append({'Severity':'N/A','CWE':'N/A','Clang Alert':0,'CodeSonar Alert':1,'Clang Rule':'N/A','CodeSonar Rule':rule,'Line':line,'True Positive':positive})
+    else:
+        
--- bugcheck2.py
+++ bugcheck2.py
@@ -1,6 +1,5 @@
 import pandas as pd
 from pandas import ExcelWriter,ExcelFile
-import numpy as np
 
 bugsdf=pd.read_excel('Juliet_Test_Suite/swampresults_edit.xlsx')
 truthdf=pd.read_excel('Juliet_Test_Suite/manifest.xlsx')
@@ -9,7 +8,11 @@ bugsdf['File Match Flag']=0
 bugsdf['CWE Match Flag']=0
 indexmap={}
 for index,row in truthdf.iterrows():
-    indexmap[row['path']]=index+2
+    path=row['path']
+    if path not in indexmap:
+        indexmap[path]={row['line']:(index+2)}
+    else:
+        indexmap[path].update({row['line']:(index+2)})
 # twentycount=0
 for index,row in bugsdf.iterrows():
     print(index)
@@ -18,19 +21,26 @@ for index,row in bugsdf.iterrows():
     path=row['Path']
     if path in indexmap:
         bugsdf.set_value(index,'File Match Flag',1)
-        truthindex=indexmap[path]
+        # truthindexs=indexmap[path]
         line=row['Line']
-        if str(row['CWE'])!='nan':
-            cwe='CWE-'+str(int(row['CWE']))
-        if truthdf.iloc[truthindex,2]==line:
+        if line in indexmap[path]:
             bugsdf.set_value(index,'True Positive Flag',1)
-            if type(truthdf.iloc[truthindex,1]) is str:
+            if str(row['CWE'])!='nan':
+                cwe='CWE-'+str(int(row['CWE']))
+            if type(truthdf.iloc[indexmap[path][line],1]) is str:
+                if cwe==truthdf.iloc[indexmap[path][line],1].split(':')[0]:
+                    bugsdf.set_value(index,'CWE Match Flag',1)
+        # for truthdex in truthindexs:
+        #     if truthdf.iloc[truthdex,2]==line:
+        #         bugsdf.set_value(index,'True Positive Flag',1)
+        #         if type(truthdf.iloc[truthdex,1]) is str:
                 # print(cwe)
                 # print(truthdf.iloc[truthindex,1].split(':')[0])
                 # twentycount+=1
-                if cwe==truthdf.iloc[truthindex,1].split(':')[0]:
+                    # if cwe==truthdf.iloc[truthdex,1].split(':')[0]:
                     # print('works')
-                    bugsdf.set_value(index,'CWE Match Flag',1)
+                    #     bugsdf.set_value(index,'CWE Match Flag',1)
+                    # break
 writer=ExcelWriter('swampresults_edit_checked.xlsx')
 bugsdf.to_excel(writer,'sheet 1',index=False)
 writer.save()
