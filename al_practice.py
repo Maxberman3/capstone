7061cb85ede29facd43a9c28c0ba0086c5553151
Maxberman3 <mb3ax@virginia.edu>
2019-07-07 16:24:42 -0400

    updated gitignore to get rid of random alipy output, pushing results of last two succesful experiments

--- al_practice.py
+++ al_practice.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from alipy.experiment import AlExperiment
+from alipy.experiment import ExperimentAnalyser
 from sklearn.linear_model import LogisticRegression
 
 df=pd.read_excel('Juliet_Test_Suite/combined_data_table.xlsx')
@@ -16,6 +17,9 @@ al=AlExperiment(X,y,model=LogisticRegression(penalty='l1',solver='liblinear'),pe
 al.split_AL(test_ratio=0.2,initial_label_rate=0.005,split_count=5,all_class=False)
 al.set_query_strategy(strategy='QueryInstanceUncertainty')
 al.set_performance_metric(performance_metric='accuracy_score')
-al.start_query(multi_thread=True)
-al.get_experiment_result()
-al.plot_learning_curve()
+al.start_query(multi_thread=False)
+# print(al.get_experiment_result())
+# al.plot_learning_curve()
+analyser=ExperimentAnalyser(x_axis='num_of_queries')
+analyser.add_method('uncertainty',al.get_experiment_result())
+analyser.plot_learning_curves()
